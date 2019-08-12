1c8de5f8a607c55926eea3d2735e72bf862af43a
gmullins925 <50452654+gmullins925@users.noreply.github.com>
2019-08-12 11:26:41 -0500

    Initial Commit

---
+++ Lab6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] Names = { "Rick Sanchez", "Morty Smith", "Jerry Smith", "Beth Smith", "Summer Smith"};
+
+			string[] Phone = { "555-1334","555-3882", "555-8211","555-1617","555-2803"};
+			string strSearch;
+
+			//prompt user
+			Console.WriteLine("Type a name to search for in the phone book: ");
+			strSearch = Console.ReadLine();
+
+			for (int n = 0; n < Names.Length; n++)
+			{
+				if (Names[n].IndexOf(strSearch) > -1)
+				{
+					Console.WriteLine(Names[n] + " -> " + Phone[n]);
+				}
+				else
+				{
+					Console.WriteLine("No such person found.");
+					break;
+				}
+			}
+			Console.ReadLine();
+		}
+	}
+}
